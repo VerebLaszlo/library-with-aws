a7859e9e5d3c058a7fa814e79f9bcafc74398e60
László Veréb <unknown@unknown.com>
2020-07-29 11:23:51 +0200

    Base network acl configuration.

--- aws/region/vpc/main.tf
+++ aws/region/vpc/main.tf
@@ -195,3 +195,111 @@ resource aws_security_group_rule all_outbound_access {
   security_group_id = aws_security_group.main.id
   cidr_blocks = ["0.0.0.0/0"]
 }
+
+resource aws_network_acl nacl-private-main {
+  vpc_id = aws_vpc.vpc-main.id
+  tags = merge({
+    Name = "nacl${var.project-name}"},
+    var.tags
+  )
+  subnet_ids = aws_subnet.private-subnet.*.id
+}
+
+/*
+resource aws_network_acl_rule naclr-inbound-all {
+  network_acl_id = aws_network_acl.nacl-private-main.id
+  rule_number = 99
+  cidr_block = "0.0.0.0/0"
+  protocol = "-1"
+  rule_action = "allow"
+  egress = false
+}
+*/
+
+resource aws_network_acl_rule naclr-inbound-http {
+  network_acl_id = aws_network_acl.nacl-private-main.id
+  rule_number = 100
+  cidr_block = "0.0.0.0/0"
+  protocol = "tcp"
+  from_port = 8080
+  to_port = 8080
+  rule_action = "allow"
+  egress = false
+}
+
+resource aws_network_acl_rule naclr-inbound-https {
+  network_acl_id = aws_network_acl.nacl-private-main.id
+  rule_number = 110
+  cidr_block = "0.0.0.0/0"
+  protocol = "tcp"
+  from_port = 443
+  to_port = 443
+  rule_action = "allow"
+  egress = false
+}
+
+resource aws_network_acl_rule naclr-inbound-ssh {
+  network_acl_id = aws_network_acl.nacl-private-main.id
+  rule_number = 120
+  cidr_block = aws_vpc.vpc-main.cidr_block
+  protocol = "tcp"
+  from_port = 22
+  to_port = 22
+  rule_action = "allow"
+  egress = false
+}
+
+resource aws_network_acl_rule naclr-inbound-response {
+  network_acl_id = aws_network_acl.nacl-private-main.id
+  rule_number = 140
+  cidr_block = "0.0.0.0/0"
+  protocol = "tcp"
+  from_port = 32768
+  to_port = 65535
+  rule_action = "allow"
+  egress = false
+}
+
+/*
+resource aws_network_acl_rule naclr-outbound-all {
+  network_acl_id = aws_network_acl.nacl-private-main.id
+  rule_number = 99
+  cidr_block = "0.0.0.0/0"
+  rule_action = "allow"
+  protocol = "-1"
+  egress = false
+}
+*/
+
+resource aws_network_acl_rule naclr-outbound-http {
+  network_acl_id = aws_network_acl.nacl-private-main.id
+  rule_number = 100
+  cidr_block = "0.0.0.0/0"
+  protocol = "tcp"
+  from_port = 80
+  to_port = 80
+  rule_action = "allow"
+  egress = true
+}
+
+resource aws_network_acl_rule naclr-outbound-https {
+  network_acl_id = aws_network_acl.nacl-private-main.id
+  rule_number = 110
+  cidr_block = "0.0.0.0/0"
+  protocol = "tcp"
+  from_port = 443
+  to_port = 443
+  rule_action = "allow"
+  egress = true
+}
+
+resource aws_network_acl_rule naclr-outbound-response {
+  network_acl_id = aws_network_acl.nacl-private-main.id
+  rule_number = 140
+  cidr_block = "0.0.0.0/0"
+  protocol = "tcp"
+  from_port = 32768
+  to_port = 65535
+  rule_action = "allow"
+  egress = true
+}
