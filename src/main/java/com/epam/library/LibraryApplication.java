933dc689a9b67539637560127433c11379c31a53
László Veréb <unknown@unknown.com>
2020-09-01 13:41:42 +0200

    Switching to war from jar.
    + Added log4j2
    + Resolving version conflicts
    + Added maven wrapper

--- src/main/java/com/epam/library/LibraryApplication.java
+++ src/main/java/com/epam/library/LibraryApplication.java
@@ -5,10 +5,12 @@ import com.epam.library.util.*;
 import org.springframework.boot.*;
 import org.springframework.boot.autoconfigure.*;
 
+// WARN: public is needed for devtools
+// WARN: do not use @ComponentScan, it will include @TestConfiguration in all tests
 @SuppressWarnings({"NonFinalUtilityClass", "UtilityClassCanBeEnum"})
 @Justification("Required spring non utility class.")
 @SpringBootApplication
-class LibraryApplication {
+public class LibraryApplication {
     public static void main(String[] args) {
         // WARN: Do not use with try-with-resource block
         SpringApplication.run(LibraryApplication.class, args);
